4faf5384fddfbe8c6362eddbb44d0dc365b3a2de
magicsli <2544510157@qq.com>
2021-03-01 14:55:01 +0800

    style: app格式代码

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <img alt="Vue logo" src="./assets/logo.png" />
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
     <Test />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import Test from './components/Test'
+// import HelloWorld from './components/HelloWorld.vue'
+import Test from "./components/Test";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld,
-    Test
-  }
-}
+    // HelloWorld,
+    Test,
+  },
+};
 </script>
 
 <style>
